23b77cf4a91aa8c68c7128a5b7269a06fc298fbb
tanis <tanis-y@hotmail.com>
2014-10-20 11:10:28 -0400

    QueryPhrase change

--- homework2/src/edu/nyu/cs/cs2580/QueryPhrase.java
+++ homework2/src/edu/nyu/cs/cs2580/QueryPhrase.java
@@ -19,60 +19,73 @@ public class QueryPhrase extends Query {
 
   public ListMultimap<String, String> _phrases = ArrayListMultimap.create();
   public Vector<String> soloTokens = new Vector<String>();
+  public boolean containsPhrase;
 
-  public QueryPhrase(String query) {
+  public QueryPhrase(String query, boolean containsPhrase) {
     super(query);
+    this.containsPhrase = containsPhrase;
   }
 
   @Override
   public void processQuery() {
-
     if (_query == null) {
       return;
     }
-    Tokenizer tokenizer;
 
-    Pattern p = Pattern.compile("(\".+?\")");
-    Matcher m = p.matcher(_query);
+    if (containsPhrase){
+      Tokenizer tokenizer;
 
-    while (m.find()) {
-      String phrase = m.group(1).replaceAll("\"", "").trim();
-      tokenizer = new Tokenizer(new StringReader(phrase));
+      Pattern p = Pattern.compile("(\".+?\")");
+      Matcher m = p.matcher(_query);
 
-      while (tokenizer.hasNext()) {
-        String term = Tokenizer.lowercaseFilter(tokenizer.getText());
-        term = Tokenizer.krovetzStemmerFilter(term);
-        _phrases.get(phrase).add(term);
-        //System.out.println(term);
-      }
-    }
+      while (m.find()) {
+        String phrase = m.group(1).replaceAll("\"", "").trim();
+        tokenizer = new Tokenizer(new StringReader(phrase));
 
-    tokenizer = new Tokenizer(new StringReader(_query));
-    HashSet<String> tokenSet = new HashSet<String>();
-    while (tokenizer.hasNext()) {
-      String term = Tokenizer.lowercaseFilter(tokenizer.getText());
-      if (term != null) {
-        term = Tokenizer.krovetzStemmerFilter(term);
-        tokenSet.add(term);
-      }
-    }
-    Set<String> phraseValueSet = new HashSet<String>();
-    phraseValueSet.addAll(_phrases.values());
-    for(String term : tokenSet){
-
-      if (phraseValueSet.contains(term)){
-        _tokens.add(term);
-      }else {
-        term = Tokenizer.stopwordFilter(term);
-        if (term != null){
-          _tokens.add(term);
-          soloTokens.add(term);
+        while (tokenizer.hasNext()) {
+          String term = Tokenizer.lowercaseFilter(tokenizer.getText());
+          term = Tokenizer.krovetzStemmerFilter(term);
+          _phrases.get(phrase).add(term);
+          //System.out.println(term);
         }
       }
 
-
+      tokenizer = new Tokenizer(new StringReader(_query));
+      HashSet<String> tokenSet = new HashSet<String>();
+      while (tokenizer.hasNext()) {
+        String term = Tokenizer.lowercaseFilter(tokenizer.getText());
+        if (term != null) {
+          term = Tokenizer.krovetzStemmerFilter(term);
+          tokenSet.add(term);
+        }
+      }
+      Set<String> phraseValueSet = new HashSet<String>();
+      phraseValueSet.addAll(_phrases.values());
+      for(String term : tokenSet){
+        if (phraseValueSet.contains(term)){
+          _tokens.add(term);
+        }else {
+          term = Tokenizer.stopwordFilter(term);
+          if (term != null){
+            _tokens.add(term);
+            soloTokens.add(term);
+          }
+        }
+      }
+    }else{
+      Tokenizer tokenizer = new Tokenizer(new StringReader(_query));
+      HashSet<String> tokenSet = new HashSet<String>();
+      while (tokenizer.hasNext()) {
+        String term = Tokenizer.lowercaseFilter(tokenizer.getText());
+        term = Tokenizer.stopwordFilter(term);
+        if (term != null) {
+          term = Tokenizer.krovetzStemmerFilter(term);
+          tokenSet.add(term);
+        }
+      }
+      for (String term : tokenSet) {
+        _tokens.add(term);
+      }
     }
   }
-
-
 }
